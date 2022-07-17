7c659768b9f3f6aab315bf1fe59662b5543e01e9
Aryan <aryan4p130202@gmail.com>
2022-07-17 11:57:20 +0530

    api updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg'
 import MovieCard from './MovieCard.jsx'
 //8064132d
 
-const API_URL = 'http://www.omdbapi.com?apikey=8064132d'
+const API_URL = 'https://www.omdbapi.com?apikey=8064132d'
 
 const movie1 = {
     Poster: "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg",
